e2e1df1e16a9d66cce17e573ec0f8f7667793692
ZJYS20 <2454951611@qq.com>
2019-04-01 21:22:46 +0800

    homework.py

--- homework.py
+++ homework.py
@@ -2,26 +2,42 @@ import requests
 import json
 import pandas as pd
 
-class Weather():
-    def get_df(cityId, cityname_dict, appcode):
-        token = '''677282c2f1b3d718152c4e25ed434bc4'''
-        appcode = '21753a4c6306468aa91b0ed3b0920bbd'
+class Weather(object):
+
+    def __init__(self, city, appcode, token):   #初始化发送请求必需的属性
+        self.city = city
+        self.appcode = appcode
+        self.token = token
+
+    def get_weather(self):
         url = 'http://freecityid.market.alicloudapi.com/whapi/json/alicityweather/briefforecast3days'
-        payload = {'cityId': cityId, 'token': token}
+        payload = {'cityId': self.city, 'token': self.token}
+        headers = {'Authorization': 'APPCODE {}'.format(self.appcode)}
+        ans_json = requests.post(url, params=payload, headers=headers)   #向API发送请求，ans_json储存返回的数据
+        ans = json.loads(ans_json.content)           #将json格式转化为List格式
+        repo = pd.DataFrame(ans['data']['forecast'])  #提取其中有用的部分，即forecast，并将其转化为数据框
+       # repo['cityname'] = city_dict[city]
+        return repo
+
+
+
+def get_weather2(city, city_dict, appcode, token):
+        url = 'http://freecityid.market.alicloudapi.com/whapi/json/alicityweather/briefforecast3days'
+        payload = {'cityId': city, 'token': token}
         headers = {'Authorization': 'APPCODE {}'.format(appcode)}
-        r = requests.post(url, params=payload, headers=headers)
-        content_json = json.loads(r.content)
-        df = pd.DataFrame(content_json['data']['forecast'])
-        df['cityname'] = cityname_dict[cityId]
-        return df
+        ans_json = requests.post(url, params=payload, headers=headers)  # 向API发送请求，ans_json储存返回的数据
+        ans = json.loads(ans_json.content)  # 将json格式转化为List格式
+        repo = pd.DataFrame(ans['data']['forecast'])  # 提取其中有用的部分，即forecast，并将其转化为数据框
+        repo['cityname'] = city_dict[city]
+        return repo
 
-
-    def get_dfs(cityname_dict, appcode):
-        dfs = []
-        for cityId in cityname_dict:
-            dfs_times = []
-            temp_df = get_df(cityId, cityname_dict, appcode)
-            dfs_times.append(temp_df)
-            city_df = pd.concat(dfs_times,ignore_index=True)
-            dfs.append(city_df)
-        return dfs
+def view(city_dict, appcode, token):
+    repos = []
+    for city in city_dict:
+        repos_times = []
+        temp_df = get_weather2(city, city_dict, appcode, token)
+        repos_times.append(temp_df)
+        city_df = pd.concat(repos_times, ignore_index=True)
+        repos.append(city_df)
+        res = pd.concat(repos, ignore_index=True)
+    return res
